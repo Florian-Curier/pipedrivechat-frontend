7ec00d6138c5102ab23c6e58e1c1657dd55ca06d
Felix <felix.dennery@gmail.com>
2024-08-07 13:23:55 +0200

    ecran alerte passage en composant

--- components/Alert.js
+++ components/Alert.js
@@ -1,6 +1,38 @@
-import styles from '../styles/Alert'
+import styles from '../styles/Alert.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash, faPen } from '@fortawesome/free-solid-svg-icons';
 
-function Alert() {
+function Alert(props) {
+
+   
+const handleClickAlertDetails = () => {
+    console.log('click see alert details')
+}
+
+const handleClickEditAlert= () => {
+    console.log('click edit alert ')
+}
+
+
+
+const handleClickDeleteAlert= () => {
+    console.log('click delete alert ')
+}
+
+
+    return (
+
+    <div className={styles.alertContainer } >
+        <button className={styles.alertName} onClick={() => handleClickAlertDetails()}>{props.name}</button>
+        <span className={styles.alertItem}> {props.trigger}</span>
+        <span className={styles.alertItem}>#{props.channel}</span>
+        <span className={styles.alertIcons}>
+            <FontAwesomeIcon icon={faPen} className={styles.edit} onClick={() => handleClickEditAlert()} />  
+            <FontAwesomeIcon icon={faTrash} className={styles.delete} onClick={() => handleClickDeleteAlert()} />
+            </span>
+    </div>
+    )
+
 
 }
 
--- components/Alerts.js
+++ components/Alerts.js
@@ -1,23 +1,28 @@
 import styles from '../styles/Alerts.module.css'
 import AppExtensionsSDK, { Command } from "@pipedrive/app-extensions-sdk";
+import Alert from './Alert';
+import ModalCreateAlert from './ModalCreateAlert';
 import { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash, faPen } from '@fortawesome/free-solid-svg-icons';
 import { faCircleQuestion } from '@fortawesome/free-regular-svg-icons';
 import { useSelector } from 'react-redux';
+import { useRouter } from 'next/router';
+
 
 
 function Alerts() {
+
     const NEXT_PUBLIC_BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL
+
     const user = useSelector((state) => state.user.value);
-    const pipedriveCompanyId = user.pipedrive_company_id;
-    const pipedriveUserId = user.pipedrive_user_id;
+    const router = useRouter()
+
     const [alertsList, setAlertsList] = useState([]);
 
 
-
-    useEffect(() => {
-        console.log('useeffect is running')
+/* 
+ useEffect(() => {
         const initializeSDK = async () => {
             try {
                 const sdk = await new AppExtensionsSDK().initialize();
@@ -29,75 +34,61 @@ function Alerts() {
         };
 
         initializeSDK();
-    }, []);
-
-    useEffect(() => {
-        fetch(`${NEXT_PUBLIC_BACKEND_URL}/alerts/${pipedriveCompanyId}/${pipedriveUserId}`)
- .then(response => response.json())
- .then(alertData => {
+    }, []); */
 
 
-setAlertsList([...alertData.alert]);
+
+useEffect(() => {
+        fetch(`${NEXT_PUBLIC_BACKEND_URL}/alerts/${user?.pipedrive_company_id}/${user?.pipedrive_user_id}`)
+        .then(response => response.json())
+        .then(alertData => {
+        setAlertsList([...alertData.alerts]);
  });
 
     }, []);
 
-    console.log(alertsList)
-    // console.log(alertsList[0].trigger_id[" trigger_name"])
+const alertsRows = alertsList.map((val, key) => {
+        return (
+            <Alert key={key} name={val.alert_name} trigger={val.trigger_id.trigger_name} channel={val.google_channel_name}/>
+        )
+    });
+          
 
-    const data = [
-        { name: "Alert 202", trigger: "New deal", channel: "equipesales" },
-        { name: "Alert 203", trigger: "Won deal", channel: "general" },
-        { name: "Alert 204", trigger: "New deal", channel: "salesteam" },
-    ]
+const handleClickNewAlert = () => {
+    console.log('click new alert')
+}
+
     return (
 
         <div className={styles.container}>
-          
-
                 <div className={styles.configcontent}>
-                    
                     <div className={styles.configheading}>
                         
                         <h2 className={styles.maintitle}>Configure my alerts</h2>
                         <p className={styles.sectionparagraph}>Create and configure custom alerts that will be sent to
                             Google Chat when specific conditions are met in Pipedrive.</p>
                     </div>
-                    <div className={styles.alertstablecontainer}>
-                        
-                    <table>
-    <thead>
-      <tr>
-        <th className={styles.paddedelements}>NAME</th>
-        <th>TRIGGER</th>
-        <th>CHANNEL</th>
-        <th>ACTION</th>
-      </tr>
-    </thead>
-    <tbody>
-      {alertsList.map((val, key) => {
-                    return (
-                        <tr key={key}>
-                            <td className={styles.paddedelements}>{val.alert_name}</td>
-                            <td>{val.trigger_id[" trigger_name"]}</td>
-                            <td>#{val.google_channel_name}</td>
-                            <td><FontAwesomeIcon icon={faPen} className={styles.edit} /><FontAwesomeIcon icon={faTrash} className={styles.delete}/></td>
-                        </tr>
-                    )
-                })}
-      <tr>
-      <td className={styles.paddedelements}><button>+New Alert</button></td>
-      <td> </td>
-      <td> </td>
-      <td> </td>
-      <td> </td>
-      </tr>
-    </tbody>
-  </table>
+                    
+                    <div className={styles.alertsTableContainer}>
+
+                    <div className={styles.tableHeader}>
+                        <span className={styles.tableHeaderTitle}>NAME</span>  
+                        <span className={styles.tableHeaderTitle}>TRIGGER</span> 
+                        <span className={styles.tableHeaderTitle}>CHANNEL</span>  
+                        <span className={styles.tableHeaderTitle}>ACTION</span>
+                    </div>
+
+                    <div className={styles.tableContent}>
+                        {alertsRows}
 
                     </div>
+
+                <div className={styles.tableNewAlert}>
+                   <button className={styles.newAlert} onClick={() => handleClickNewAlert()}>+New Alert</button></div> 
+              
+                   </div>
+
                     <div className={styles.configfooter}>
-                        
                         <div className={styles.helpsection}>
                         <p className={styles.helptext}>Need Help</p>
                         <FontAwesomeIcon icon={faCircleQuestion} className={styles.helpicon} />
