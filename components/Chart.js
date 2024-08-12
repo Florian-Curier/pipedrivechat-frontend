01978fc65f64fddb005bb8f72f4a82325aaee957
thegreatinternetlord <msg.johan.navarro@gmail.com>
2024-08-12 12:26:50 +0200

    affichage du turnover (composant chart)

--- components/Chart.js
+++ components/Chart.js
@@ -5,11 +5,14 @@ import { Chart as ChartJS } from "chart.js/auto";
 
 function Chart(props) {
     if(props.chartType === 'Bar') {
-    return <Bar data={props.chartData} type={props.chartType} />;
+        
+    return <> <h2>{props.chartTitle}</h2><Bar data={props.chartData} type={props.chartType} /> </>;
+    
     }
 
     if(props.chartType === 'Pie') {
         return <Pie data={props.chartData} type={props.chartType} />;
+        
     }
 
 }
