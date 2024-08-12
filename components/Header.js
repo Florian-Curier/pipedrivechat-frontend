b488449664f2e62714a9f335c9d2891b014984f4
Florian <florian.curier.57@gmail.com>
2024-08-12 09:41:13 +0200

    ajout de la popover

--- components/Header.js
+++ components/Header.js
@@ -1,34 +1,67 @@
-import { useEffect, useState } from 'react';
-import styles from '../styles/Header.module.css'
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-
+import { useEffect, useState } from "react";
+import styles from "../styles/Header.module.css";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import "antd/dist/antd.css";
+import { Popover, Button } from "antd";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { useSelector } from "react-redux";
 
 function Header() {
+  const userEmail = useSelector((state) => state.user.value.google_email);//accéder à l'adresse mail de l'utilisateur stocké dans l'état global de redux
 
-const router = useRouter();
+  const popoverContent = (
+    //affichage de l'adresse mail de l'utilisateur
+    <div className={styles.popoverContent}>
+      <p>{userEmail}</p>
+    </div>
+  );
 
-const path = router.pathname
+  const router = useRouter();
 
-useEffect(() => {
+  const path = router.pathname;
 
+  useEffect(() => {}, [router]);
 
-},[router] )
-
-    return (
-        <header className={styles.container}>
-			<Link href="/alerts">
-             <span className={`${styles.link} ${path ==='/alerts' ? styles.active : ''}`}>Alerts</span>
-            </Link>
-            <Link href="/dashboard">
-            <span  className={`${styles.link} ${path ==='/dashboard' ? styles.active : ''}`}>Dashboard Buisness</span>
-            </Link>
-            <Link href="/stats">
-            <span className={`${styles.link} ${path ==='/stats' ? styles.active : ''}`}>Usage statistics</span>
-            </Link>
-            <span  className={styles.userIcon}>User button</span>
-		</header >
-    )
+  return (
+    <header className={styles.container}>
+      <Link href="/alerts">
+        <span
+          className={`${styles.link} ${
+            path === "/alerts" ? styles.active : ""
+          }`}
+        >
+          Alerts
+        </span>
+      </Link>
+      <Link href="/dashboard">
+        <span
+          className={`${styles.link} ${
+            path === "/dashboard" ? styles.active : ""
+          }`}
+        >
+          Dashboard Buisness
+        </span>
+      </Link>
+      <Link href="/stats">
+        <span
+          className={`${styles.link} ${path === "/stats" ? styles.active : ""}`}
+        >
+          Usage statistics
+        </span>
+      </Link>
+      <Popover
+        className={styles.popover}
+        content={popoverContent}
+        title="Adresse-mail"
+      >
+        <Button shape="round">
+          <FontAwesomeIcon icon={faUser} />
+        </Button>
+      </Popover>
+    </header>
+  );
 }
 
 export default Header;
